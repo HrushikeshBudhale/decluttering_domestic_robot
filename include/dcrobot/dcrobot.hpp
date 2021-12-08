a9f03c610b3193fb8aaf66168c68f5bbb0b42b1b
ameyakonk <ameyakonkar@gmail.com>
2021-12-08 20:31:00 +0000

    [Task 3.07] Added headers in DcRobot class

--- include/dcrobot/dcrobot.hpp
+++ include/dcrobot/dcrobot.hpp
@@ -10,9 +10,29 @@
  */
 #pragma once
 
-#include <navigation/navigation.hpp>
-#include <grasp_object/grasp_object.hpp>
-#include <detect_object/detect_object.hpp>
+#include <actionlib/client/simple_action_client.h>
+#include <geometry_msgs/PointStamped.h>
+#include <control_msgs/PointHeadAction.h>
+#include <tf2_ros/transform_listener.h>
+// #include <geometry_msgs/Pose.h>
+
+// C++ standard headers
+#include <string>
+
+// Boost headers
+#include <boost/shared_ptr.hpp>
+
+#include "navigation/navigation.hpp"
+#include "grasp_object/grasp_object.hpp"
+#include "detect_object/detect_object.hpp"
+#include "object_spawner/object_spawner.hpp"
+
+
+// Action interface type for moving TIAGo's head, provided as a typedef
+// for convenience
+typedef actionlib::SimpleActionClient<control_msgs::PointHeadAction>
+                                                            PointHeadClient;
+typedef boost::shared_ptr<PointHeadClient> PointHeadClientPtr;
 
 class DCRobot {
  public:
