c11306166e1333d7e82d27b7ee2d2e1ad71bf780
ameyakonk <ameyakonkar@gmail.com>
2021-12-08 16:20:00 +0000

    [Task 3.04] Created Constructer for GraspObject class

--- src/grasp_object.cpp
+++ src/grasp_object.cpp
@@ -12,8 +12,9 @@
 
 #include <grasp_object/grasp_object.hpp>
 
-GraspObject::GraspObject() {
-     nh_ = nodeHandle;
+GraspObject::GraspObject(ros::NodeHandle* nodeHandle) {
+    //  Initializing parameter values
+    nh_ = nodeHandle;
     //  Initializing Service client
     set_object_state_client_ = nh_->serviceClient<std_srvs::SetBool>(
                                                             "/setObjectState");
@@ -21,22 +22,4 @@ GraspObject::GraspObject() {
 }
 
 
-void GraspObject::openGrip() {
-    // function responsible for openig the grip
-}
-
-
-void GraspObject::closeGrip() {
-    // function responsible for closing the grip
-}
-
-
-void GraspObject::pickObject(geometry_msgs::Pose) {
-    // function responsible for picking up the object given its pose
-}
-
-
-void GraspObject::placeObject(geometry_msgs::Pose) {
-    // function responsible for placing the object given the pose of place
-}
 
