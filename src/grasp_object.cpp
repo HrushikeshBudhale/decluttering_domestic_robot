ce817610ce55d6cd821ce14a51295bd816f7a200
ameyakonk <ameyakonkar@gmail.com>
2021-12-08 16:50:00 +0000

    [Task 3.04] Created and implemented move_arm_to_pose method

--- src/grasp_object.cpp
+++ src/grasp_object.cpp
@@ -21,5 +21,51 @@ GraspObject::GraspObject(ros::NodeHandle* nodeHandle) {
     ROS_INFO_STREAM("[GraspObject] GraspObject object initialized");
 }
 
+void GraspObject::move_arm_to_pose(geometry_msgs::Pose inPose) {
+    //  create am asynchronous spinner
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+
+    //  publishing robot arm goal pose
+    geometry_msgs::PoseStamped goal_pose;
+    goal_pose.header.frame_id = "base_footprint";
+    goal_pose.pose = inPose;
+    moveit::planning_interface::MoveGroupInterface group_arm_torso("arm_torso");
+    group_arm_torso.setPlannerId("SBLkConfigDefault");
+    group_arm_torso.setPoseReferenceFrame("base_footprint");
+    group_arm_torso.setPoseTarget(goal_pose);
+
+    ROS_INFO_STREAM("[GraspObject] Planning to move " <<
+                    group_arm_torso.getEndEffectorLink()
+                    << " to a target pose expressed in " <<
+                    group_arm_torso.getPlanningFrame());
+
+    group_arm_torso.setStartStateToCurrentState();
+    group_arm_torso.setMaxVelocityScalingFactor(1.0);
+
+    //  Create moveit planner interface.
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+    group_arm_torso.setPlanningTime(5.0);
+    bool success = static_cast<bool>(group_arm_torso.plan(my_plan));
+
+    if (!success)
+        throw std::runtime_error("No plan found");
+
+    ROS_INFO_STREAM("[GraspObject] Plan found in " << my_plan.planning_time_
+                                                                << " seconds");
+
+    // Execute the plan
+    ros::Time start = ros::Time::now();
+
+    moveit::planning_interface::MoveItErrorCode e = group_arm_torso.move();
+    if (!static_cast<bool>(e))
+        throw std::runtime_error("Error executing plan");
+
+    ROS_INFO_STREAM("[GraspObject] Motion duration: "
+                                        << (ros::Time::now() - start).toSec());
+    spinner.stop();
+}
+
 
 
