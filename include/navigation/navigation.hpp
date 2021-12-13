c1778c91ba9350de434197a52f185e4a84d8c202
Abhijit Mahalle <abhijeetmahalle27@gmail.com>
2021-12-13 14:08:04 -0500

    Task [2.20] Added doxygen documentation

--- include/navigation/navigation.hpp
+++ include/navigation/navigation.hpp
@@ -26,15 +26,67 @@
 // Std C++ headers
 #include <vector>
 
+
 class Navigation {
  public:
+    /**
+     * @brief Construct a new Navigation:: Navigation object
+     *        Creates publisher and subscriber objects     
+     * 
+     * @param node_handle
+     * @return void 
+     */
     explicit Navigation(ros::NodeHandle*);
+
+    /**
+     * @brief Updates new checkpoints as goal for the robot.
+     * @return void
+     */
     void set_next_checkpoint_as_goal();
+    
+    /**
+     * @brief sets bin location as goal for the robot 
+     * @return void
+     */
     void set_bin_location_as_goal();
+
+    /**
+     * @brief Callback function to obtain current robot position and orientation
+     * 
+     * @param robot_pose
+     * @return void 
+     */
     void robot_pose_cb(const geometry_msgs::PoseWithCovarianceStamped&);
+    
+    /**
+     * @brief Checks if a given is reached.
+     * 
+     * @return true if goal reached.
+     * @return false if goal is not reached.
+     */
     bool is_goal_reached();
+
+    /**
+     * @brief turns the robot by 180 degree 
+     *        or till the object is detected. 
+     * @return void
+     */
+    void turn_around();
+
+    /**
+     * @brief Stops moving the robot once the robot has reached within a 
+     *        certain distance from the object.
+     * @return void
+     */
     void stop_moving();
+    
+    /**
+     * @brief Sets object coordinates as a goal to the robot
+     * @return void
+     * @param objectPose 
+     */
     void set_object_pose_as_goal(geometry_msgs::Pose);
+    
     enum turning {
         TURN_START,
         TURNING,
@@ -43,22 +95,45 @@ class Navigation {
     turning turn_state;
 
  private:
-    void initialize_checkpoint_list();
+   
+    /**
+     * @brief Published turning velocity to the robot.
+     * @return void 
+     */
     void set_turning_velocity();
 
+    /**
+     * @brief initialize predefined checkpoints
+     * @return void
+     */
+    void initialize_checkpoint_list();
+
+    /** Node handle  */
     ros::NodeHandle* nh_;
+    /** subscriber to get current robot pose  */
     ros::Subscriber cur_pose_sub_;
+    /** Publisher to set checkpoint goals  */
     ros::Publisher goal_pub_;
+    /** Publisher to set turning velocity  of the robot */
     ros::Publisher vel_pub_;
+    /** Publisher to cancel a given goal  */
     ros::Publisher cancel_goal_pub_;
+    /** ServiceClient to clear cost map  */
     ros::ServiceClient clear_cost_map_client_;
 
+    /** checks if robot pose is received  */
     bool is_pose_initialized_;
 
+    /** stores current robot pose  */
     geometry_msgs::Pose current_pose_;
+    /** stores the target bin location  */
     geometry_msgs::Pose bin_location_;
+    /** stores the goal pose for the robot  */
     geometry_msgs::Pose goal_pose_;
+    /** stores initial robot orientation for the robot to rotate  */
     tf2::Quaternion initial_tf_quat_;
+    /** stores fixed checkpoints for the robot  */
     std::vector <geometry_msgs::Pose> checkpoints_;
+    /** iterator to change checkpoints  */
     std::vector<geometry_msgs::Pose>::size_type checkpoint_counter_;
 };
